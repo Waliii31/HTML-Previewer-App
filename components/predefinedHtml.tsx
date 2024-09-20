a0f32d053b049c3b92f0f78e4c268d30af7d4d65
wali <waliizafri@gmail.com>
2024-09-20 17:43:21 +0500

    first commit

---
+++ components/predefinedHtml.tsx
@@ -0,0 +1,12 @@
+
+export const predefinedHtml = `
+  <div>
+    <h2>Welcome to the HTML Previewer!</h2>
+    <p>This is a sample predefined HTML content. You can edit this or paste your own HTML.</p>
+    <ul>
+      <li>HTML Previewer</li>
+      <li>Supports dynamic preview</li>
+      <li>Predefined HTML sample</li>
+    </ul>
+  </div>
+`;
